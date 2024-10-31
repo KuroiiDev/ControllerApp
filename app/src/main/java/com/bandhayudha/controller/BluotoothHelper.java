74fb56958e517263ebb3ea7c640b2560a983763a
KuroiiDev <maitsamkadzim26@gmail.com>
2024-10-31 17:00:24 +0700

    Adding few Bluetooth things

---
+++ app/src/main/java/com/bandhayudha/controller/BluotoothHelper.java
@@ -0,0 +1,9 @@
+package com.bandhayudha.controller;
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+public class BluotoothHelper {
+}
