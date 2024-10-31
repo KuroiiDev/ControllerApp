9cc87be0c8f60119ad5b28352a474e9b330ae37f
KuroiiDev <maitsamkadzim26@gmail.com>
2024-10-31 17:01:30 +0700

    Variables

--- app/src/main/java/com/bandhayudha/controller/BluotoothHelper.java
+++ app/src/main/java/com/bandhayudha/controller/BluotoothHelper.java
@@ -5,5 +5,13 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
 public class BluotoothHelper {
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket bluetoothSocket;
+    private OutputStream outputStream;
 }
