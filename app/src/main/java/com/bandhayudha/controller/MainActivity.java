f4ff011d02163f6d887afe585d24584c8806c75c
KuroiiDev <maitsamkadzim26@gmail.com>
2024-10-31 17:14:12 +0700

    Adding Check Bluetooth

--- app/src/main/java/com/bandhayudha/controller/MainActivity.java
+++ app/src/main/java/com/bandhayudha/controller/MainActivity.java
@@ -16,23 +16,47 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.widget.Toast;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CODE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         if (!hasBluetoothPermissions()) {
             requestBluetoothPermissions();
+        } else {
+            initializeBluetoothConnection();
+        }
+    }
+
+    private void checkBluetoothEnabled() {
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            // Bluetooth is not enabled; request to enable it
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         } else {
             initializeBluetoothConnection();
         }
@@ -67,6 +91,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeBluetoothConnection() {
-        // INGET KODE INIT TARUH SINII
+        Toast.makeText(this, "Working!", Toast.LENGTH_SHORT).show();
     }
 }
